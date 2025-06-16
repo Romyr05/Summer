acfed40d210685278691b94ad0b6242ffbbcee1e
Romyr <romyrlopez123@gmail.com>
2025-06-17 02:20:05 +0800

    test 1

---
+++ prac_tinder_java/advanced_functions/practice/trial.js
@@ -0,0 +1,15 @@
+
+/*
+const materials = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
+
+const get_length = (material) => {
+  return material.length
+}
+
+
+console.log(materials.map(get_length));
+// Expected output: Array [8, 6, 7, 9]
+
+*/
+
+const num = document.querySelector('.move-button').innerHTML = 'testing';
