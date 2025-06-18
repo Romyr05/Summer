82b86749cd61dc75512af4ecf7d171bce2451c63
Romyr <romyrlopez123@gmail.com>
2025-06-19 02:40:37 +0800

    made uploading pics

---
+++ tinderSwipe/script.js
@@ -0,0 +1,24 @@
+
+    //here it needs to be different pictures to run
+var file_upload = function (event){
+    //appends each image to the children
+    for(let i = 0; i< event.target.files.length;i++){
+        var image = document.createElement('img')
+        image.src = URL.createObjectURL(event.target.files[i])
+        document.getElementById('preview-img').appendChild(image)
+    }
+}
+
+/*
+document.getElementById('upload-file').addEventListener("change",function() {
+    const file = this.files[0];
+    const prevImg = document.getElementById('preview-img')
+    prevImg.src = URL.createObjectURL(file);
+    console.log("File Selected:", file.name)
+    })
+*/
+
+
+function pic_clicked(){
+    document.getElementById('upload-file').click()
+}
