d790ce7ec999ef75dc36229da8ff3c9d5aa09b7a
Romyr <romyrlopez123@gmail.com>
2025-06-21 04:52:56 +0800

    What is left is the swiping and css and this is sort of done

--- tinderSwipe/script.js
+++ tinderSwipe/script.js
@@ -60,7 +60,10 @@ document.getElementById('keep').addEventListener("click",() => {
     if(currentIndex < queueImage.length)
     {
         imageKeep.push(queueImage[currentIndex]);
-        console.log(imageKeep)
+        const keep = document.createElement('img');
+        keep.src = imageKeep[currentIndex].url
+        document.getElementById('keeped-img').append(keep)
+        console.log(imageKeep);
         currentIndex++
         display_image()
         track_queue++;
@@ -84,6 +87,31 @@ function remove_all (array){
     array.splice(0,array.length)
 }
 
+console.log(imageKeep);
+
+
+if(track_queue === queueImage.length)
+{
+    imageKeep.forEach((keeped,index) => {
+    console.log(keeped)
+    const keep = document.createElement('img');
+    keep.src = imageKeep[index].url
+    document.getElementById('keeped-img').appendChild(keep[index])
+})
+}
+
+let clicked = false
+
+document.getElementById('check-keep').addEventListener("click",()=>{
+    if(!clicked){
+        document.getElementById('keeped-img').style.display = 'block';
+        clicked = true
+    }else{
+        document.getElementById('keeped-img').style.display = 'none';
+        clicked = false
+    }
+    
+})
 
 
 
