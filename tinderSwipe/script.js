2fa447a439b9e1a5ecf9b1e82a98f5b9e22902ab
Romyr <romyrlopez123@gmail.com>
2025-06-21 04:10:41 +0800

    aaaa

--- tinderSwipe/script.js
+++ tinderSwipe/script.js
@@ -3,43 +3,86 @@ function pic_clicked(){
     document.getElementById('upload-file').click()
 }
 
-    queueImage= [];
+imageKeep = [];
+queueImage= [];
+ImageAll = [];
+let currentIndex = 0;
+let track = 0;
+let track_queue = 0;
 
-    document.getElementById('upload-file').addEventListener("change",function(){
-        //upload the image in the frontend
-        const files = Array.from(this.files)
+document.getElementById('upload-file').addEventListener("change",function(){
+    //upload the image in the frontend
+    const files = Array.from(this.files)
 
-        files.forEach((file,index) =>{
-
-            const picture = URL.createObjectURL(file);
-            queueImage.push({file,url:picture})
-
-            console.log(index)
-            const image = document.createElement('img')
-            image.src = picture;
-            image.id = `image-${index}`;
-
-            if(index === 0)
-            {
-                document.getElementById('test').src = picture;
-            }
-
-            console.log(image);
-            document.getElementById('preview-img').appendChild(image);
-        })
-    })
-    //id is not updating
-
-    stored = [];
-
-
-    function keep(){
-        document.getElementById('keep').addEventListener("click",() =>{
-            console.log("asaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-            stored.push(queueImage[0])
-            document.getElementById('test123').src = queueImage[0].url;
-        })
+    if(track_queue === queueImage.length)
+    {
+        console.log("running")
+        remove_all(queueImage);
+        console.log(`rawr ${queueImage}`)
     }
+    track_queue = 0;
+
+    files.forEach((file,index) =>{
+        const picture = URL.createObjectURL(file);
+        queueImage.push({file,url:picture})
+        ImageAll.push({file,url:picture})
+        const image = document.createElement('img');
+        if(track < ImageAll.length)
+        {
+            image.src = ImageAll[track].url
+            track++;
+        }
+        document.getElementById('preview-img').append(image)
+    })
+    document.getElementById('upload-file').value = ""
+    currentIndex = 0;
+    console.log(`track: ${track_queue}`)
+
+    display_image()
+
+
+})
+
+function display_image() {
+    if(currentIndex < queueImage.length)
+    {
+        console.log(`display queue: ${(queueImage.length)}`);
+        console.log(`display: currentINdex: ${(currentIndex)}`);
+        document.getElementById('test123').src = queueImage[currentIndex].url
+        track_queue++
+    }else
+    {
+        document.getElementById('test123').src = ""
+    }
+}
+
+document.getElementById('keep').addEventListener("click",() => {
+    if(currentIndex < queueImage.length)
+    {
+        imageKeep.push(queueImage[currentIndex]);
+        console.log(imageKeep)
+        currentIndex++
+        display_image()
+        track_queue++;
+    }
+})
+
+
+document.getElementById('delete').addEventListener("click", () => {
+    if(currentIndex < queueImage.length)
+    {
+        queueImage[currentIndex] = null;
+        console.log(`queue: ${(queueImage.length)}`);
+        console.log(`currentIndex: ${(currentIndex)}`);
+        currentIndex++
+        display_image()
+
+    }
+})
+
+function remove_all (array){
+    array.splice(0,array.length)
+}
 
 
 
